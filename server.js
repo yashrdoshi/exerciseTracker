83ba9035c2a09c80593e41aedf6fcd0912cfad4c
yashrdoshi <yashdoshi99.04@gmail.com>
2021-07-02 13:19:14 +0530

    changed server.js

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.use('/users',usersRouter)
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,'/frontend/build')))
 
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'frontend','build','index.html'))
+    app.get('/*',(req,res)=>{
+        res.sendFile(path.join(__dirname+'frontend/build/index.html'))
         // res.sendFile(path.join(__dirname,'frontend','build','index.html'))
     })
 }else{
